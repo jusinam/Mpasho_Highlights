4a043cac910132e0c479249ca4f2eb12ee1031a4
DjCooGie <evansonchagwa01@gmail.com>
2020-01-01 23:29:14 +0300

    import the operating system(os) module, create a config class and two other config subclasses, add environment variables and define functions that get API and SECRET keys and create a dictionary that accesses different configuration classes

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey=fc44e252019a4642b8194a9006c9a279'
+    CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=fc44e252019a4642b8194a9006c9a279'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=fc44e252019a4642b8194a9006c9a279'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+     Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+     Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

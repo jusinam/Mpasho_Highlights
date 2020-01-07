011416beb9294471dbd04882937fe16c0e2c411d
DjCooGie <evansonchagwa01@gmail.com>
2020-01-07 10:08:50 +0300

    added styling

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     General configuration parent class
     '''
     
-    TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey=fc44e252019a4642b8194a9006c9a279'
-    CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=fc44e252019a4642b8194a9006c9a279'
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=fc44e252019a4642b8194a9006c9a279'
+    TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey=0cfbe113f27e4c7cb88279e437d8b67d'
+    CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=0cfbe113f27e4c7cb88279e437d8b67d'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=0cfbe113f27e4c7cb88279e437d8b67d'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
